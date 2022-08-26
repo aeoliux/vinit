89ba04b3969d6d9f8953c6d7fc3ada58deb00e29
zapomnij <crewmateminecraft@gmail.com>
2022-08-26 12:11:07 +0200

    Added stop script for static network connection

--- doc/main.md
+++ doc/main.md
@@ -3,5 +3,6 @@
 Sections:
 - [Installation](install.md)
 - [Explaining inittab](inittab.md)
+- [vinit linuxrc documentation](linuxrc/main.md)
 
 If you want to use vinit with OpenRC instead of using SysVinit or openrc-init. Check about [OpenRC](openrc.md).
--- linuxrc/rc.halt.sh
+++ linuxrc/rc.halt.sh
@@ -46,14 +46,6 @@ if [ -n "${SERVICES}" ]; then
 	done
 fi
 
-if [ -w /proc/sysrq-trigger ]; then
-	echo "=> Sending SIGKILL to all processes"
-	echo "i" > /proc/sysrq-trigger
-
-	echo "=> Sending SIGTERM to all processes"
-	echo "e" > /proc/sysrq-trigger
-fi
-
 echo "=> Syncing disks"
 sync
 
@@ -65,3 +57,8 @@ umount -a
 
 echo "=> Remounting rootfs as read-only"
 mount -o ro,remount /
+
+if [ -w /proc/sysrq-trigger ]; then
+	echo "=> Sending SIGTERM to all processes"
+	echo "e" > /proc/sysrq-trigger
+fi
